88e6604188e125563b594694b73cdf6d9e7c64b4
Kim Sarkies <meaculpa123ks@hotmail.com>
2011-04-03 13:56:23 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apple123::Application.config.secret_token = 'eb203a72710fdf8cf74877569183adf88d74762177c5aed04f6c39a1dcc4166d2a499df9ef17d96bfba0cf28473dfae688004ec5c1d9c97250a7b7831f78769b'
